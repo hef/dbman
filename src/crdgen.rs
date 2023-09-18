f69c85ff8ea75ea211a0feb92509053f16c5857c
hef <hef@pbrfrat.com>
2023-09-17 22:55:04 -0500

    fix(crd): generating better crds

--- src/crdgen.rs
+++ src/crdgen.rs
@@ -3,7 +3,7 @@ use kube::CustomResourceExt;
 
 fn main() {
     print!(
-        "{}{}",
+        "{}\n---\n{}",
         serde_yaml::to_string(&controller::Database::crd()).unwrap(),
         serde_yaml::to_string(&controller::DatabaseServer::crd()).unwrap()
     )
