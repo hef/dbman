78cd7f2c0c141b61865adb4f1c6bda705f2db014
hef <hef@pbrfrat.com>
2024-01-20 03:06:56 -0600

    fix(crd): combining crd versions again

--- src/crdgen.rs
+++ src/crdgen.rs
@@ -4,6 +4,7 @@ use controller::v1alpha1;
 use controller::v1alpha2;
 use controller::v1alpha3;
 use k8s_openapi::apiextensions_apiserver::pkg::apis::apiextensions::v1::CustomResourceDefinition;
+use k8s_openapi::apiextensions_apiserver::pkg::apis::apiextensions::v1::CustomResourceDefinitionVersion;
 use kube::CustomResourceExt;
 
 #[derive(Parser, Debug)]
@@ -23,15 +24,31 @@ fn main() {
         // go template syntax is `{{- if .Values.crds.enabled }}`
         println!("{{{{- if .Values.crds.enabled }}}}");
     }
+    let mut database = v1alpha3::Database::crd();
+
+    let versions = vec![
+        v1alpha3::Database::crd().spec.versions[0].clone(),
+        v1alpha2::Database::crd().spec.versions[0].clone(),
+    ];
+    database.spec.versions = versions;
+
+    let mut database_server = v1alpha2::DatabaseServer::crd();
+    let versions = vec![
+        v1alpha2::DatabaseServer::crd().spec.versions[0].clone(),
+        v1alpha1::DatabaseServer::crd().spec.versions[0].clone(),
+    ];
+    database_server.spec.versions = versions;
 
     println!("---");
     let crds: Vec<CustomResourceDefinition> = vec![
         // old
-        v1alpha1::DatabaseServer::crd(),
-        v1alpha2::Database::crd(),
+        //v1alpha1::DatabaseServer::crd(),
+        //v1alpha2::Database::crd(),
         // current
-        v1alpha3::Database::crd(),
-        v1alpha2::DatabaseServer::crd(),
+        //v1alpha3::Database::crd(),
+        //v1alpha2::DatabaseServer::crd(),
+        database,
+        database_server,
     ];
     crds.iter().for_each(|crd| {
         print!(
